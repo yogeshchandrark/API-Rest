b2f757127be27efccf4487feba629dd01073ca17
Yogesh <r.yogeshchandra@gmail.com>
2019-07-13 20:17:14 +0530

    added put post patch and delete methods

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -1,6 +1,11 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from profiles_api import views
+
+router = DefaultRouter()
+router.register('helloViewset', views.HellowViewSet, base_name='helloViewset')
 
 urlpatterns = [
     path('helloworld/', views.HelloApiView.as_view()),
+    path('', include(router.urls)),
 ]
