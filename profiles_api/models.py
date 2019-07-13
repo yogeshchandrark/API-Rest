0cc390253eee3474d702d5d311aebd4176850eaf
Yogesh <r.yogeshchandra@gmail.com>
2019-07-13 18:47:46 +0530

    admin ready for user profile

---
+++ profiles_api/models.py
@@ -0,0 +1,62 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+
+
+class UserProfileManager(BaseUserManager):
+    """Manager for user profiles """
+
+    def create_user(self, email, name, password=None):
+        """Creates a new user with the given detials."""
+
+        # Check that the user provided an email.
+        if not email:
+            raise ValueError('Users must have an email address.')
+
+        # Create a new user object.
+        user = self.model(email=self.normalize_email(email), name=name,)
+
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, name, password):
+        """Creates and saves a new superuser with given detials."""
+
+        # Create a new user with the function we created above.
+        user = self.create_user(
+            email,
+            name,
+            password
+        )
+
+        # Make this user an admin.
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+
+        return user
+
+
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active =models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserProfileManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+    def get_full_name(self):
+        """Retrive name from the user """
+        return self.name
+
+    def get_short_name(self):
+        return self.name
+
+    def __str__ (self):
+        """ Return String representation of our user"""
+        return self.email
